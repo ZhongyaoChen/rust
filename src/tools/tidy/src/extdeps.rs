cca183cb1f33fd5d8757ced3c301821431e0446b
Zhongyao Chen <chen.zhongyao@zte.com.cn>
2025-09-03 15:55:16 +0800

    fix libffi-rs fail when cross-compile for rva23 dist

--- src/tools/tidy/src/extdeps.rs
+++ src/tools/tidy/src/extdeps.rs
@@ -8,6 +8,7 @@ const ALLOWED_SOURCES: &[&str] = &[
     r#""registry+https://github.com/rust-lang/crates.io-index""#,
     // This is `rust_team_data` used by `site` in src/tools/rustc-perf,
     r#""git+https://github.com/rust-lang/team#a5260e76d3aa894c64c56e6ddc8545b9a98043ec""#,
+    r#""git+https://github.com/ZhongyaoChen/libffi-rs#f6a601912f9b1c59868dd6654e8fdd1941fa9570""#,
 ];
 
 /// Checks for external package sources. `root` is the path to the directory that contains the
